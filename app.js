43cb954d2499fe6d921323b7d018484611b7debf
ajay91py <166129570+ajay91py@users.noreply.github.com>
2024-07-26 18:33:56 +0530

    changed display colors and added clear button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let previousValue='';
 let currentValue='';
 let operator='';
-document-addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
    let numbers=document.querySelectorAll('.number');
    let operators=document.querySelectorAll('.operator')
    let previousNumber=document.querySelector('.previous')
@@ -14,6 +14,7 @@ document-addEventListener('DOMContentLoaded', ()=>{
    numbers.forEach((btn)=> btn.addEventListener('click', (e)=>{getNumbers(e.target.textContent)}))
    operators.forEach((op)=> op.addEventListener('click', (e)=>{getOperator(e.target.textContent)}))
    equal.addEventListener('click', ()=>{result(previousValue,operator,currentValue)})
+   clear.addEventListener('click', clearscrn)
    
    function getNumbers(x){
       currentValue+=x;
@@ -22,28 +23,28 @@ document-addEventListener('DOMContentLoaded', ()=>{
 
    function getOperator(y){
       operator+=y;
-      console.log(operator);
+      
       previousValue=currentValue;
       if(!previousNumber.textContent.includes('+','-','*','/'))
       previousNumber.textContent=previousValue+' '+operator;
       currentValue='';
    }
 function result(a,op,b) {
-   console.log('equal is clicked');
+  
     a=Number(previousValue)
     b=Number(currentValue)
    switch(op){
       case '+':
          previousNumber.textContent=a+b;
-         currentNumber.textContent=''
+         currentNumber.textContent=' '
          break;
          case '-':
             previousNumber.textContent=a-b;
-            currentNumber.textContent=''
+            currentNumber.textContent=' '
             break;
             case '*':
             previousNumber.textContent=a*b;
-            currentNumber.textContent=''
+            currentNumber.textContent=' '
             break;
             case '/':
             previousNumber.textContent=a/b;
@@ -51,6 +52,13 @@ function result(a,op,b) {
             break;
 }
 
+}
+function clearscrn(){
+previousNumber.textContent=''
+currentNumber.textContent=''
+previousValue=''
+currentValue=''
+operator=''
 }
 })
 
